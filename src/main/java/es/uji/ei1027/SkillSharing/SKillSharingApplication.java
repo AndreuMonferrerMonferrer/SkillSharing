dd1b0500e790173d29355978e8661537ad8f6fe9
aarongp11 <aarongarciapitarch@gmail.com>
2022-03-06 19:38:17 +0100

    aaa

--- src/main/java/es/uji/ei1027/SkillSharing/SKillSharingApplication.java
+++ src/main/java/es/uji/ei1027/SkillSharing/SKillSharingApplication.java
@@ -3,8 +3,12 @@ package es.uji.ei1027.SkillSharing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.logging.Logger;
+
 @SpringBootApplication
 public class SKillSharingApplication {
+
+	private static final Logger log = Logger.getLogger(SKillSharingApplication.class.getName());
 
 	public static void main(String[] args) {
 		SpringApplication.run(SKillSharingApplication.class, args);

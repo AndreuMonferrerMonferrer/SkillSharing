2b3a0aaf19f367ac585d4ce41d3267280614aa3b
al394804 <al394804@uji.es>
2022-04-25 19:19:34 +0200

    rootcontroller

---
+++ src/main/java/es/uji/ei1027/SkillSharing/controller/RootController.java
@@ -0,0 +1,15 @@
+package es.uji.ei1027.SkillSharing.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class RootController {
+    @RequestMapping(value="")
+    public String homePage() {
+        return "index";
+    }
+
+}
